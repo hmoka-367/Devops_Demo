aac90482b83e11dd35218e94840ae83d5786ec0a
hemanth369m <hemanth369m@gmail.com>
2025-02-13 13:14:09 +0530

    added Junit tests

---
+++ src/main/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2, "2 + 2 should equal 4");
+    }
+}
